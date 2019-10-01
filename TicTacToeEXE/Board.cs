6abd2a6b391bde7a6f86c84194f5418efb8e9aaf
LukeM7 <48161289+LukeM7@users.noreply.github.com>
2019-10-01 12:43:57 -0700

    Add files via upload

---
+++ TicTacToeEXE/Board.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace TicTacToeEXE
+{
+    public class Board
+    {
+        public Program program = new Program();
+
+        public string[,] Tiles = {
+                {"1", "2", "3"},
+                {"4", "5", "6"},
+                {"7", "8", "9"}
+        };
+
+        public string[,] newTiles = {
+                {"1", "2", "3"},
+                {"4", "5", "6"},
+                {"7", "8", "9"}
+        };
+
+        public bool ChangeTiles(string input, string player)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (newTiles[i, j].Equals(input))
+                    {
+                        if (Tiles[i, j].Equals("X") || Tiles[i, j].Equals("O"))
+                        {
+                            Console.WriteLine("Cannot choose slot already taken");
+                            return false;
+                        }
+                        else
+                        {
+                            Tiles[i, j] = player;
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool isWinner()
+        {
+            if (Tiles[0, 0].Equals("X") && Tiles[0, 1].Equals("X") && Tiles[0, 2].Equals("X"))
+                return true;
+            else if (Tiles[1, 0].Equals("X") && Tiles[1, 1].Equals("X") && Tiles[1, 2].Equals("X"))
+                return true;
+            else if (Tiles[2, 0].Equals("X") && Tiles[2, 1].Equals("X") && Tiles[2, 2].Equals("X"))
+                return true;
+            else if (Tiles[0, 0].Equals("X") && Tiles[1, 0].Equals("X") && Tiles[2, 0].Equals("X"))
+                return true;
+            else if (Tiles[0, 1].Equals("X") && Tiles[1, 1].Equals("X") && Tiles[2, 1].Equals("X"))
+                return true;
+            else if (Tiles[0, 2].Equals("X") && Tiles[1, 2].Equals("X") && Tiles[2, 2].Equals("X"))
+                return true;
+            else if (Tiles[0, 0].Equals("X") && Tiles[1, 1].Equals("X") && Tiles[2, 2].Equals("X"))
+                return true;
+            else if (Tiles[0, 2].Equals("X") && Tiles[1, 1].Equals("O") && Tiles[0, 2].Equals("O"))
+                return true;
+            else if (Tiles[0, 0].Equals("O") && Tiles[0, 1].Equals("O") && Tiles[0, 2].Equals("O"))
+                return true;
+            else if (Tiles[1, 0].Equals("O") && Tiles[1, 1].Equals("O") && Tiles[1, 2].Equals("O"))
+                return true;
+            else if (Tiles[2, 0].Equals("O") && Tiles[2, 1].Equals("O") && Tiles[2, 2].Equals("O"))
+                return true;
+            else if (Tiles[0, 0].Equals("O") && Tiles[1, 0].Equals("O") && Tiles[2, 0].Equals("O"))
+                return true;
+            else if (Tiles[0, 1].Equals("O") && Tiles[1, 1].Equals("O") && Tiles[2, 1].Equals("O"))
+                return true;
+            else if (Tiles[0, 2].Equals("O") && Tiles[1, 2].Equals("O") && Tiles[2, 2].Equals("O"))
+                return true;
+            else if (Tiles[0, 0].Equals("O") && Tiles[1, 1].Equals("O") && Tiles[2, 2].Equals("O"))
+                return true;
+            else if (Tiles[0, 2].Equals("O") && Tiles[1, 1].Equals("O") && Tiles[0, 2].Equals("O"))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ TicTacToeEXE/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace TicTacToeEXE
+{
+    public class Program
+    {
+        static Board game = new Board();
+        public static int turncounter;
+
+        static void Main()
+        {
+            int x = 1;
+            turncounter = 0;
+            Intro();
+            while (x == 1)
+            {
+                pickPlayer(turncounter);
+                PrintTiles();
+                getInput();
+                if (game.isWinner())
+                {
+                    Console.WriteLine("Congrats {0} You've Won!", pickPlayer(--turncounter));
+                    ResetGame();
+                }
+                else
+                    continue;
+                Console.Read();
+            }
+        }
+
+        public static void Intro()
+        {
+            Console.WriteLine("Welcome to Tic Tac Toe");
+            Console.WriteLine("O Goes First, Press q anytime to quit");
+        }
+
+        static void PrintTiles()
+        {
+            Console.WriteLine("{0}    {1}    {2}", game.Tiles[0, 0], game.Tiles[0, 1], game.Tiles[0, 2]);
+            Console.WriteLine("===========");
+            Console.WriteLine("{0}    {1}    {2}", game.Tiles[1, 0], game.Tiles[1, 1], game.Tiles[1, 2]);
+            Console.WriteLine("===========");
+            Console.WriteLine("{0}    {1}    {2}", game.Tiles[2, 0], game.Tiles[2, 1], game.Tiles[2, 2]);
+        }
+
+        public static string pickPlayer(int turncounter)
+        {
+            // White is represented by 1, Black by 0
+            if (turncounter % 2 == 0)
+            {
+                return "O";
+            }
+            else
+                return "X";
+        }
+
+        static void getInput()
+        {
+            Console.WriteLine("Please press the number you wish to select");
+            string input = Console.ReadLine();
+            if (input.Equals("q") || input.Equals("Q"))
+                ExitGame();
+            bool checkAnswer = game.ChangeTiles(input, pickPlayer(turncounter));
+            if (checkAnswer)
+            {
+                turncounter++;
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Another Number");
+                return;
+            }
+        }
+
+        static void ResetGame()
+        {
+            game.Tiles = game.newTiles;
+            Main();
+        }
+
+        static void ExitGame()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
